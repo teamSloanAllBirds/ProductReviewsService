7c87e426489e5086062a9d3b8e26e254818b349b
Nathan Vu <mr.nathanvu@gmail.com>
2020-08-19 10:33:54 -0700

    fix css-loader & make changes for proxy

--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -3,7 +3,7 @@ const models = require('../models/models');
 module.exports = {
   get: (req, res) => {
     var id = req.url.split('/')[3];
-    console.log('ID', id);
+    // console.log('ID', id);
     models.getAll(id, (err, results) => {
       if (err) {
         console.log('SERVER GET FAILS', err);
